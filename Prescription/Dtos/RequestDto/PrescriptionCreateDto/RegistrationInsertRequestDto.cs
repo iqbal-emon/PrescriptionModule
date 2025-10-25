da906c6aa17bea85ae87cc0fe20bd5fba8bfa7c9
iqbal-emon <ihemon281@gmail.com>
2025-10-26 01:29:17 +0600

    dfada

---
+++ Prescription/Dtos/RequestDto/PrescriptionCreateDto/RegistrationInsertRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prescription.Dtos.RequestDto.PrescriptionCreateDto
+{
+    public class RegistrationInsertRequestDto
+    {
+        public int doctorId { get; set; }
+        public DoctorDto? Doctor { get; set; }
+    }
+
+
+}
